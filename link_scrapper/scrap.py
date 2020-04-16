ed169dd60237d7a7c17c33093d7ce8f6e2309ce7
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-04-16 20:51:29 +0530

    Making recursive link scrapper

--- link_scrapper/scrap.py
+++ link_scrapper/scrap.py
@@ -2,27 +2,6 @@ import requests
 import bs4
 
 
-# # Selecting the type of data we want
-# f = open("link_scrapper/data.txt", "w+")
-# for i in soup.find_all('a', href=True):
-#     flow = i['href']
-#     if flow.startswith('https://') or flow.startswith('http://'):
-#         f.write(flow + "\n")
-#     elif flow.startswith('/wiki/File:'):
-#         new_flow = "https://en.wikipedia.org" + str(flow)
-#         f.write(new_flow + "\n")
-#     elif flow.startswith('//'):
-#         new_flow = "https:" + str(flow)
-#         f.write(new_flow + "\n")
-#     elif flow.startswith('/') or flow.startswith('#'):
-#         new_flow = "https://en.wikipedia.org/wiki/Black_hole" + str(flow)
-#         f.write(new_flow + "\n")
-#     else:
-#         new_flow = "https://en.wikipedia.org/wiki/Black_hole/" + str(flow)
-#         f.write(new_flow + "\n")
-# f.close()
-
-
 class RightbrosDataScrapper:
     """
     Simple Data Scrapper from a url
@@ -85,11 +64,11 @@ class RightbrosDataScrapper:
         return "Scrapping Done. Please check the file."
 
     
-url = "https://en.wikipedia.org/wiki/Computer"
-file_path = "link_scrapper/computer.txt"
-obj = RightbrosDataScrapper(url, file_path)
+# url = "https://en.wikipedia.org/wiki/Computer"
+# file_path = "link_scrapper/computer.txt"
+# obj = RightbrosDataScrapper(url, file_path)
 
-"""
-Need to call only one method of the class write_data()
-"""
-obj.write_data()
+# """
+# Need to call only one method of the class write_data()
+# """
+# obj.write_data()
